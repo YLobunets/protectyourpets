a24d56bc7938042b4f890d6c56448b9c24ad2f89
Iurii Lobunets <iurii@lobunets.com.ua>
2022-11-16 16:33:43 +0200

    Replacе quotes in django files

--- logbook/apps.py
+++ logbook/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class LogbookConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'logbook'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "logbook"
